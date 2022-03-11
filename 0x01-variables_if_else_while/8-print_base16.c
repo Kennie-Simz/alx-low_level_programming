c65061a57f0bf31ef47b77362c6cfa3eeb7f3a9f
Kennedy Nyongesa <48585401+Kennie-Simz@users.noreply.github.com>
2022-03-11 23:27:32 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,22 +2,30 @@
 
 /**
  * main - prints all the numbers of base 16 in lowercase,
+ *
  * followed by a new line
+ *
  * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
 	int n;
+	
 	char letters;
 
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
+	
 	for (letters = 'a'; letters <= 'f'; letters++)
 	{
 		putchar(letters);
 	}
+	
 	putchar('\n');
+	
 	return (0);
 }
