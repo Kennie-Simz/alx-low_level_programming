2086d7e3900ee1a748aa694f421273f2c26b81c3
Kennie-Simz <nyongesa17@outlook.com>
2022-03-11 07:46:01 -0800

    file 4-puts created

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-
 /**
  *  main - Entry point
  *
- *  Return: Always 0 (success)
+ *  Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	returns (0);
+
+	char str0[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(str0);
+	return (0);
 }
