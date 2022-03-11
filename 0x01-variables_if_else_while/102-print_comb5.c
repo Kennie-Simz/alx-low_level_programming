0e1f8c18779c4d5d33716ce69456b40df2b84a2b
Kennedy Nyongesa <48585401+Kennie-Simz@users.noreply.github.com>
2022-03-11 23:23:10 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,6 +2,7 @@
 
 /**
  * main - prints all possible combinations of two two-digit numbers
+ 
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,13 +16,19 @@ int main(void)
 			if (i < j)
 			{
 				putchar((i / 10) + 48);
+				
 				putchar((i % 10) + 48);
+				
 				putchar(' ');
+				
 				putchar((j / 10) + 48);
+				
 				putchar((j % 10) + 48);
+				
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
+					
 					putchar(' ');
 				}
 			}
@@ -29,5 +36,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
+	
 	return (0);
 }
