c88514397c3a02f228285e91ac42b18ac747de3d
Kennie-Simz <nyongesa17@outlook.com>
2022-03-16 09:01:23 -0700

    fixed file

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -15,9 +15,9 @@ int main(void)
 int a, b, c;
 int largest;
 
-a = 0;
-b = -2;
-c = -98;
+a = 972;
+b = -98;
+c = 0;
 
 largest = largest_number(a, b, c);
 printf("%d is the largest number\n", largest);
