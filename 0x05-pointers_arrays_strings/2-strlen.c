13043fdc398d8dd9acd1e10b2ba791d827d1855e
Kennie-Simz <nyongesa17@outlook.com>
2022-03-21 14:12:09 -0700

    length of strings function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Finds the length of a given string
+ * @s: address of first character in the string
+ *
+ * Return: length of the string
+ **/
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+	{
+		int length = 0;
+		s++;
+	}
+
+	return(length);
+}
