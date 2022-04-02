d77c653639f0c82784c0cc673aee07c008b1144f
Kennedy Nyongesa <48585401+Kennie-Simz@users.noreply.github.com>
2022-04-03 00:27:19 +0300

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -11,5 +11,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	return (0);
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
 }
