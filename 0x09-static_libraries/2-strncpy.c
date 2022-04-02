2eb3b44de73bfc285553a741f190a96b5f07b12a
Kennedy Nyongesa <48585401+Kennie-Simz@users.noreply.github.com>
2022-04-03 00:31:44 +0300

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -12,5 +12,22 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	return (0);
+	int i = 0;
+	char *new_dest = dest;
+
+	/* copy at most n characters to dest */
+	while (*src && (i < n))
+	{
+		*dest++ = *src++;
+		i++;
+	}
+
+	/* ensure than n bytes are written */
+	while (i < n)
+	{
+		*dest++ = '\0';
+		i++;
+	}
+
+	return (new_dest);
 }
