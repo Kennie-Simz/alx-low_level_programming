8590925c92b9e1cf25e3f0801f5e41376d64c23e
Kennedy Nyongesa <48585401+Kennie-Simz@users.noreply.github.com>
2022-04-03 00:36:00 +0300

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -11,5 +11,16 @@
 
 char *_strchr(char *s, char c)
 {
-	return (0);
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (c == s[i])
+			return (s + i);
+	}
+
+	if (c == s[i])
+		return (s + i);
+
+	return (NULL);
 }
