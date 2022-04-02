2828ea32d506d98b19fc508a4119c4d3662c018a
Kennedy Nyongesa <48585401+Kennie-Simz@users.noreply.github.com>
2022-04-03 00:21:48 +0300

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - prints some function
+ * _isalpha - prints alphabetic characters
  * @c: returned characters
  *
  * Return: Aways 0
@@ -10,5 +10,8 @@
 
 int _isalpha(int c)
 {
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	
 	return (0);
 }
