ee4ccbf06b1de0008b31ec176e290067a9ce08fc
Kennedy Nyongesa <48585401+Kennie-Simz@users.noreply.github.com>
2022-04-03 00:29:22 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -10,5 +10,42 @@
 
 int _atoi(char *s)
 {
-	return (0);
+	int count_dash = 0;
+	int found_num = 0;
+	unsigned int value = 0;
+
+	while (*s)
+	{
+		/* check if you have found a number */
+		if (found_num)
+		{
+			/* break if the current character is not a digit */
+			if (*s < '0' || *s > '9')
+				break;
+			/* if the current number is a digit, add it to the value found */
+			else
+				value = (value * 10) + (*s - '0');
+		}
+		else
+		{
+			/* check if the current character is not a digit */
+			if (*s < '0' || *s > '9')
+			{
+				if (*s == '-')
+					count_dash++;
+			}
+			else
+			{
+				value = (*s - '0');
+				found_num = 1;
+			}
+		}
+		s++;
+	}
+
+	/* check if the found number is negative */
+	if (found_num && (count_dash % 2))
+		value = -value;
+
+	return (value);
 }
