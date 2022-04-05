24a66256fc0beec908f844cd282324d9bf9e8b5a
Kennie-Simz <nyongesa17@outlook.com>
2022-04-05 12:55:21 -0700

    Keanu Reeves

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a 2-dimensional array of ints
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: a pointer to an array of integers,
+ * and NULL if width or height is 0 or -ve, or on failure,
+ *
+ **/
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid = (int **)malloc(sizeof(int *) * height);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	/*initialize all values in the array to 0*/
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,6 +5,6 @@ char *create_array(unsigned int size, char c);
 int _strlength(char *s);
 char *_strdup(char *s);
 char *str_concat(char *s1, char *s2);
-
+int **alloc_grid(int width, int height);
 
 #endif
