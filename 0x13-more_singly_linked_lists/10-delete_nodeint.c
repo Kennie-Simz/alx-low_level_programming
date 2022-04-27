055e2ad448d08a38120eeddf619850fa6b707430
Kennie-Simz <nyongesa17@outlook.com>
2022-04-27 01:15:30 -0700

    fix task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	current = *head;
-	for (i= 0; i < index - 1; i++)
+	for (i = 0; i < index - 1; i++)
 	{
 		if (current->next == NULL)
 			return (-1);
