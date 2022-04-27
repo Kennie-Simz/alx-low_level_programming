8077d619de0adaa559615cdfbeb754dcf4e73230
Kennie-Simz <nyongesa17@outlook.com>
2022-04-27 00:55:13 -0700

    fix task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,73 +4,46 @@
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: Double pointer to the actual position of the linked list (struct)
- * @idx: Position for the new node
+ * @idx: Index of the node to insert
  * @n: Number to be passed to the new node
  *
- * Return: Sum of the value of all nodes
+ * Return: Pointer to the indexed node
  *
  **/
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	listint_t *new;
-	listint_t *tmp = *head;
+	unsigned int i;
+	listint_t *current, *new;
 
 	if (head == NULL)
 		return (NULL);
 
-	if (idx == 0)
+	if (idx != 0)
 	{
-		tmp = add_nodeint(head, n);
-		return (tmp);
-	}
-
-	while (i < idx)
-	{
-		tmp = tmp->next;
-		if (tmp == NULL)
+		current = *head;
+		for (i = 0; i < idx - 1 && current != NULL; i++)
+		{
+			current = current->next;
+		}
+		if (current == NULL)
 			return (NULL);
-		i++;
 	}
-
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->n = tmp->n;
-	new->next = tmp->next;
-	tmp->n = n;
-	tmp->next = new;
-
-	return (tmp);
-}
-
-/**
- * add_nodeint - adds a new node at the beginning of a list_t list
- * @head: Pointer to the actual position of the linked list (struct)
- * @n: value to include in the new node
- *
- * Return: Size of the linked_list
- *
- **/
-
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-	new->next = *head;
-	*head = new;
 
-	return (*head);
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	new->next = current->next;
+	current->next = new;
+	return (new);
 }
