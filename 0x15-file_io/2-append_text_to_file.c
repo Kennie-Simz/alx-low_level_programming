9c71663d456dfc4f173c181b2324500991f776d1
Kennie-Simz <nyongesa17@outlook.com>
2022-05-03 05:05:51 -0700

    fix task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -39,7 +39,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == 1)
 		return (-1);
-	if (text_content !+= NULL)
+	if (text_content != NULL)
 		len = write(fd, text_content, _strlen(text_content));
 	close(fd);
 	if (len == -1)
